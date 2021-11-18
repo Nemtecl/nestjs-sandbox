ad51fbec9f9b6eb8d46bf7f28330129679fedd2b
Clément Drouin <c.drouin@proxiad.com>
2021-11-19 00:11:19 +0100

    feat: part one

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { PostsModule } from './posts/posts.module';
 @Module({
-  imports: [],
+  imports: [PostsModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
